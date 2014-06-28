b81d691d2d3b3938966e1365f47d029192f8e4c3
brettin <brettin@anl.gov>
2014-06-27 19:45:22 -0500

    creating new setup for bamutil

---
+++ vagrant_bamutil/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# set up for gnko
+
+apt-get update
+apt-get install -y build-essential
+apt-get install -y git
+apt-get install -y cmake
+apt-get install -y libncurses5-dev
+apt-get install -y libncurses5
+apt-get install -y dh-autoreconf
+apt-get install -y pkg-config
+
+
+git clone https://github.com/statgen/bamUtil.git
+pushd bamUtil
+make cloneLib
+make
+make install INSTALLDIR=/usr/local/bin
