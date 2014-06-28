2f7617f5dc66c830c3712645040faee4295f236f
brettin <brettin@anl.gov>
2014-06-27 21:15:23 -0500

    added logic to wrap and deploy java jars into sh scipts

--- vagrant_picard/bootstrap.sh
+++ vagrant_picard/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set up for gnko
+# set up for java 
 
 apt-get update
 apt-get install -y openjdk-7-jdk
@@ -10,3 +10,17 @@ apt-get install unzip
 wget http://downloads.sourceforge.net/project/picard/picard-tools/1.115/picard-tools-1.115.zip
 unzip picard-tools-1.115.zip
 
+pushd picard-tools-1.115
+
+for n in *.jar ; do 
+  p=$(basename $n .jar) ; 
+  echo "/usr/bin/java -jar /usr/local/jars/picard-1.115/$n " > /usr/local/bin/$p.sh ;
+  chmod a+rx /usr/local/bin/$p.sh ;
+done
+
+
+mkdir -p /usr/local/jars/picard-1.115
+chmod -R a+rx /usr/local/jars
+
+cp * /usr/local/jars/picard-1.115
+chmod a+r /usr/local/jars/picard-1.115/*
