94891b09fee6b810464c370d2e19ca0222ce87f4
brettin <brettin@anl.gov>
2014-08-17 17:43:41 -0400

    fixed bad wget option

--- vagrant_jellyfish/bootstrap.sh
+++ vagrant_jellyfish/bootstrap.sh
@@ -9,7 +9,7 @@ apt-get install -y libncurses5
 apt-get install -y dh-autoreconf
 apt-get install -y pkg-config
 
-wget -s ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz
+wget -q ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz
 gunzip jellyfish-2.1.3.tar.gz
 tar -xvf jellyfish-2.1.3.tar 
 pushd jellyfish-2.1.3/
