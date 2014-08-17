74bbb6ce52642c30bdcabfc5fde7de8a9bd9d3e0
brettin <brettin@anl.gov>
2014-08-17 18:53:55 -0400

    new files

---
+++ vagrant_freebayes/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# set up for freebayes 
+
+apt-get update
+apt-get install -y build-essential
+apt-get install -y git
+apt-get install -y cmake
+apt-get install -y libncurses5-dev
+apt-get install -y libncurses5
+apt-get install -y dh-autoreconf
+apt-get install -y pkg-config
+
+git clone --recursive git://github.com/ekg/freebayes.git
+
+pushd freebayes
+make
+make install
+popd
