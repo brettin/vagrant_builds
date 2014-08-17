8f13366f1b8a3a7cf9d051f458fb7bce9a03a84d
brettin <brettin@anl.gov>
2014-08-17 17:34:41 -0400

    added ftp download of tarball to bootstrap.sh

--- vagrant_jellyfish/bootstrap.sh
+++ vagrant_jellyfish/bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# set up for gnko
-
 apt-get update
 apt-get install -y build-essential
 apt-get install -y git
@@ -11,7 +9,8 @@ apt-get install -y libncurses5
 apt-get install -y dh-autoreconf
 apt-get install -y pkg-config
 
-# TODO - how to get jellyfish
+wget -s ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz
+gunzip jellyfish-2.1.3.tar.gz
 tar -xvf jellyfish-2.1.3.tar 
 pushd jellyfish-2.1.3/
 ./configure --prefix=/usr/local
