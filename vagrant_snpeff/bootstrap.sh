383d38838a038e6e1c32092e5855f94e4435b55d
brettin <brettin@anl.gov>
2014-09-09 22:49:27 -0500

    new vag

---
+++ vagrant_snpeff/bootstrap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# set up for java 
+
+apt-get update
+apt-get install -y openjdk-7-jdk
+#apt-get install -y ant
+apt-get install unzip
+
+wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
+unzip snpEff_latest_core.zip
+
+cat > $dst <<EOF
+#---
+# Databases are stored here
+# E.g.: Information for 'hg19' is stored in data_dir/hg19/
+#
+# You can use tilde ('~') as first character to refer to your home directory. 
+# Also, a non-absolute path will be relative to config's file dir
+# 
+#---
+data.dir = ./data/
+EOF
+
+
+
+for n in *.jar ; do 
+  p=$(basename $n .jar) ; 
+  echo "\"/usr/bin/java\" \"-jar\" \"/usr/local/jars/picard-1.115/$n\" \"\$@\"" > /usr/local/bin/$p.sh ;
+  chmod a+rx /usr/local/bin/$p.sh ;
+done
+
+
+mkdir -p /usr/local/jars/picard-1.115
+chmod -R a+rx /usr/local/jars
+
+cp * /usr/local/jars/picard-1.115
+chmod a+r /usr/local/jars/picard-1.115/*
