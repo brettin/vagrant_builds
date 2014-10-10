9ee8070601d5329037ef3b7231cdddbde79ea4c5
brettin <brettin@anl.gov>
2014-10-10 16:43:46 -0400

    get bootstrap in working order, w/o installing databases

--- vagrant_snpeff/bootstrap.sh
+++ vagrant_snpeff/bootstrap.sh
@@ -27,29 +27,13 @@ wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
 unzip snpEff_latest_core.zip
 
 
-# cat > $target <<EOF
-#---
-# Databases are stored here
-# E.g.: Information for 'hg19' is stored in data_dir/hg19/
-#
-# You can use tilde ('~') as first character to refer to your home directory. 
-# Also, a non-absolute path will be relative to config's file dir
-# 
-#---
-# data.dir = $dst/data/
-# EOF
+cp -r snpEff /usr/local
+pushd /usr/local/snpEff
 
+for n in *.jar ; do 
+   p=$(basename $n .jar) ; 
+   echo "\"/usr/bin/java\" \"-jar\" \"/usr/local/snpEff/$n\" \"\$@\"" > /usr/local/bin/$p.sh ;
+   chmod a+rx /usr/local/bin/$p.sh ;
+done
 
-
-# for n in *.jar ; do 
-#   p=$(basename $n .jar) ; 
-#   echo "\"/usr/bin/java\" \"-jar\" \"/usr/local/jars/picard-1.115/$n\" \"\$@\"" > /usr/local/bin/$p.sh ;
-#   chmod a+rx /usr/local/bin/$p.sh ;
-# done
-
-
-# mkdir -p /usr/local/jars/picard-1.115
-# chmod -R a+rx /usr/local/jars
-
-# cp * /usr/local/jars/picard-1.115
-# chmod a+r /usr/local/jars/picard-1.115/*
+popd
