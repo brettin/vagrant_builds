c8ceae4c4e58b0d800e470e73a069378d6411a8e
brettin <brettin@anl.gov>
2015-01-13 18:08:30 -0600

    added new vagrant build for glam2

---
+++ vagrant_glam2/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# build script for mugsy
+
+target=${TARGET-/usr/local}
+
+if [[ $# -ne 0 ]] ; then
+        target=$1
+        shift
+fi
+
+
+apt-get update
+apt-get install -y build-essential
+apt-get install -y git
+apt-get install -y cmake
+apt-get install -y libncurses5-dev
+apt-get install -y libncurses5
+apt-get install -y dh-autoreconf
+apt-get install -y pkg-config
+
+# mugsy specific stuff
+
+apt-get install -y glam2
+
