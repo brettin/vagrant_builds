d996d5b01c80060a18e13efd16bc53b0840ae86b
brettin <brettin@anl.gov>
2016-09-28 16:53:40 -0500

    new module for keras

---
+++ trusty64/vagrant_karas/bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+target=${TARGET-/usr/local}
+
+if [[ $# -ne 0 ]] ; then
+        target=$1
+        shift
+fi
+
+
+apt-get update
+apt-get install -y git
+apt-get install -y build-essential
+apt-get install -y dh-autoreconf
+apt-get install -y pkg-config
+
+
+apt-get install -y python-numpy 
+apt-get install -y python-scipy
+aot-get install -y python-dev
+apt-get install -y python-pip
+apt-get install -y python-nose
+apt-get install -y g++
+apt-get install -y libopenblas-dev
+apt-get install -y libfreetype6-dev
+
+pip install Theano
+pip install pandas
+pip install matplotlib
+pip install sklearn # not absolutely required
+
+# python -c "import numpy; numpy.test()"
+# python -c "import scipy; scipy.test()"
+# python -c "import theano; theano.test()"
+# python `python -c "import os, theano; print os.path.dirname(theano.__file__)"`/misc/check_blas.py
+
+git clone https://github.com/fchollet/keras.git
+cd keras
+git checkout master
+python setup.py install
+
+
