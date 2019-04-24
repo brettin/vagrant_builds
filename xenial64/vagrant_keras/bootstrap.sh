784af9d856bbe90ae80bd774c762288b207d644e
brettin <brettin@anl.gov>
2019-04-23 18:42:20 -0700

    new bootstrap

---
+++ xenial64/vagrant_keras/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+target=${TARGET-/usr/local}
+
+if [[ $# -ne 0 ]] ; then
+        target=$1
+        shift
+fi
+
+
+apt-get update
+apt-get install -y git
+apt-get install -y build-essential
+apt-get install -y dh-autoreconf
+apt-get install -y pkg-config
+
+
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.sh
+
