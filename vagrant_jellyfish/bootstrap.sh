ccda728f8650ed877b41558a3d6b6083e62ad857
brettin <brettin@anl.gov>
2019-10-16 07:51:51 -0500

    upgraded jellyfish

--- vagrant_jellyfish/bootstrap.sh
+++ vagrant_jellyfish/bootstrap.sh
@@ -9,10 +9,18 @@ apt-get install -y libncurses5
 apt-get install -y dh-autoreconf
 apt-get install -y pkg-config
 
-wget -q -o wget.log ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz
-gunzip jellyfish-2.1.3.tar.gz
-tar -xvf jellyfish-2.1.3.tar 
-pushd jellyfish-2.1.3/
+wget https://github.com/gmarcais/Jellyfish/releases/download/v2.3.0/jellyfish-2.3.0.tar.gz
+# wget -q -o wget.log ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz
+
+gunzip jellyfish-2.3.0.tar.gz
+# gunzip jellyfish-2.1.3.tar.gz
+
+# tar -xvf jellyfish-2.1.3.tar 
+tar -xvf jellyfish-2.3.0.tar
+
+# pushd jellyfish-2.1.3/
+pushd jellyfish-2.3.0
+
 ./configure --prefix=/usr/local
 make
 make install
